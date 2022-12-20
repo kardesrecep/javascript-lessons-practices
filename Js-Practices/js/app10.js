aa65c94fdc4d64a1b3bbba72c81f6b766363a4e6
kardesrecep <recepkardes89@gmail.com>
2022-12-20 17:35:45 +0300

    20/12/2022

--- Js-Practices/js/app10.js
+++ Js-Practices/js/app10.js
@@ -1,2 +1,12 @@
 //Büyük sayının küçük sayıya bölümünden kalanlar toplamını, küçük sayı büyük sayıya
 //yetişene kadar birer birer artırarak bulalım.
+
+let sayi1El=document.querySelector("#sayi1");
+let sayi2El=document.querySelector("#sayi2");
+let mesaj= document.querySelector("#msg");
+
+document.querySelector("#hesapla").addEventListener("click", ()=>{
+    let sayi1=sayi1El.value;
+    let sayi2=sayi2El.value;
+
+});
